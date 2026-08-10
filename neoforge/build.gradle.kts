d1009df1ccac221e4fe5326c117896e540e17f57
PTOM76 <ptms76@outlook.jp>
2026-08-10 13:56:21 +0900

    chore: ci workflow add neoforge

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("dev.architectury.loom")
     id("architectury-plugin")
     id("com.gradleup.shadow")
+    id("me.modmuss50.mod-publish-plugin") version "2.2.0"
 }
 
 val neoforge_version = project.findProperty("neoforge_version") as String
@@ -53,16 +54,19 @@ dependencies {
 tasks.processResources {
     val mcDep = project.findProperty("mc_dep") as String
     val neoforgeLoaderDep = project.findProperty("neoforge_loader_dep") as String
+    val javafmlDep = project.findProperty("javafml_dep") as String
 
     inputs.property("version", project.version)
     inputs.property("mc_dep", mcDep)
     inputs.property("neoforge_loader_dep", neoforgeLoaderDep)
+    inputs.property("javafml_dep", javafmlDep)
 
     filesMatching("META-INF/neoforge.mods.toml") {
         expand(
             "version" to project.version,
             "mc_dep" to mcDep,
             "neoforge_loader_dep" to neoforgeLoaderDep,
+            "javafml_dep" to javafmlDep,
         )
     }
 }
@@ -74,4 +78,35 @@ tasks.shadowJar {
 
 tasks.remapJar {
     inputFile.set(tasks.shadowJar.flatMap { it.archiveFile })
+}
+
+publishMods {
+    val mcVersion = project.name
+    file.set(tasks.remapJar.flatMap { it.archiveFile })
+    displayName.set(tasks.remapJar.flatMap { it.archiveFile.map { it.asFile.name } })
+    changelog.set("Release of version ${project.version} for Minecraft $mcVersion (NeoForge)")
+    type.set(me.modmuss50.mpp.ReleaseType.STABLE)
+    modLoaders.add("neoforge")
+
+    val curseforgeId = project.findProperty("curseforge_project_id")?.toString()
+    val curseforgeToken = providers.environmentVariable("CURSEFORGE_TOKEN").orNull ?: project.findProperty("curseforge_token")?.toString()
+    if (!curseforgeId.isNullOrEmpty() && !curseforgeToken.isNullOrEmpty()) {
+        curseforge {
+            projectId.set(curseforgeId)
+            accessToken.set(curseforgeToken)
+            minecraftVersions.add(mcVersion)
+            client.set(true)
+            server.set(true)
+        }
+    }
+
+    val modrinthId = project.findProperty("modrinth_project_id")?.toString()
+    val modrinthToken = providers.environmentVariable("MODRINTH_TOKEN").orNull ?: project.findProperty("modrinth_token")?.toString()
+    if (!modrinthId.isNullOrEmpty() && !modrinthToken.isNullOrEmpty()) {
+        modrinth {
+            projectId.set(modrinthId)
+            accessToken.set(modrinthToken)
+            minecraftVersions.add(mcVersion)
+        }
+    }
 }
